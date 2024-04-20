ba97744c103f7571e29d04228292b3528e8f4194
Ankeraout <ankeraout@yahoo.com>
2024-04-20 11:50:32 +0200

    bootstrap64: clear .bss section

--- bootloader/doc/bootstrap64.md
+++ bootloader/doc/bootstrap64.md
@@ -3,5 +3,6 @@ bootstrap64 is a very small program written in x86_64 assembly that initializes
 the execution environment for executing C code. It performs the following
 initializations steps:
 - Initialize the stack pointer
+- Clear the `.bss` section
 - Call `main`, with the address of the system information structure as
 the first parameter (in the `rdi` register)
--- bootloader/src/bootstrap64.asm
+++ bootloader/src/bootstrap64.asm
@@ -2,12 +2,20 @@ section .text
 bits 64
 
 extern main
-extern puts
+extern __bss_start
+extern __bss_end
+extern memset
 
 global main64
 main64:
     ; Prepare stack
     mov rsp, 0x9f000
+
+    ; Clear .bss
+    mov rdi, __bss_start
+    mov rdx, __bss_end
+    sub rdx, rdi
+    xor rsi, rsi
 
     ; Pass the pointer to the system information structure as a parameter
     ; NOTE: this will clear the 32 upper bits of RDI
