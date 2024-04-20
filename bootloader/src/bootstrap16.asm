8be5345738031ae62633178da5fb12880390a7a3
Ankeraout <ankeraout@yahoo.com>
2024-04-20 22:34:07 +0200

    Improved bootstrap16's code quality

--- bootloader/src/bootstrap16.asm
+++ bootloader/src/bootstrap16.asm
@@ -14,33 +14,34 @@ _start:
     xor sp, sp
     jmp 0x1000:main16
 
-%include "bootstrap16/cpu.inc"
-%include "bootstrap16/stdio.inc"
-%include "bootstrap16/strings.inc"
-%include "bootstrap16/a20.inc"
-%include "bootstrap16/nmi.inc"
-%include "bootstrap16/gdt.inc"
-%include "bootstrap16/mmap.inc"
-%include "bootstrap16/sysinfo.inc"
-%include "bootstrap16/vbe.inc"
-
 main16:
-    mov ax, str_announce
-    push ax
-    call puts
-    add sp, 2
-
+    ; Check the CPU
     call cpu_check
-
-cpu x64
-    call a20_init
-
     test ax, ax
-    jnz .a20_error
+    jnz .error_cpu
 
+    ; After this point, we know that the CPU is supported, so we can use 32-bit
+    ; instructions.
+    cpu x64
+
+    ; Enable the A20 gate
+    call a20_enable
+    test ax, ax
+    jnz .error_a20
+
+    ; Initialize VBE (note that there is no error checking, because vbe_init
+    ; failing is not considered a fatal error).
     call vbe_init
+
+    ; Initialize the memory map.
     call mmap_init
+    test ax, ax
+    jnz .error_mmap
+
+    ; Initialize the system information structure.
     call sysinfo_init
+    test ax, ax
+    jnz .error_sysinfo
 
     ; Disable interrupts (including NMI)
     cli
@@ -68,12 +69,39 @@ cpu x64
 
     jmp dword 0x0008:main32
 
-.a20_error:
-    push word str_a20_nok
-    call puts
-    add sp, 2
+cpu 8086
+    .error_cpu:
+        mov ax, str_error_cpu
+        jmp .fatal_error
+        
+    .error_a20:
+        mov ax, str_error_a20
+        jmp .fatal_error
 
-halt:
-    cli
-    hlt
-    jmp halt
+    .error_mmap:
+        mov ax, str_error_mmap
+        jmp .fatal_error
+
+    .error_sysinfo:
+        mov ax, str_error_sysinfo
+        jmp .fatal_error
+
+    .fatal_error:
+        push ax
+        call puts
+        add sp, 2
+
+    .halt:
+        cli
+        hlt
+        jmp .halt
+
+%include "bootstrap16/a20.inc"
+%include "bootstrap16/cpu.inc"
+%include "bootstrap16/gdt.inc"
+%include "bootstrap16/mmap.inc"
+%include "bootstrap16/nmi.inc"
+%include "bootstrap16/stdio.inc"
+%include "bootstrap16/strings.inc"
+%include "bootstrap16/sysinfo.inc"
+%include "bootstrap16/vbe.inc"
