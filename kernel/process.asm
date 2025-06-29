9493964ba772dcbfa0ffa759957dbb6c1c9637b5
Ankeraout <ankeraout@yahoo.com>
2025-06-29 20:22:39 +0200

    kernel/process.asm: Added process_destroy()

--- kernel/process.asm
+++ kernel/process.asm
@@ -7,6 +7,8 @@ struc ts_process
     .m_memoryAllocationListOffset: resw 1
     .m_memoryAllocationListSegment: resw 1
     .m_codeSegment: resw 1
+    .m_childListOffset: resw 1
+    .m_childListSegment: resw 1
 endstruc
 
 section .text
@@ -60,6 +62,10 @@ process_new:
     mov es:[di + ts_process.m_memoryAllocationListSegment], ax
     mov es:[di + ts_process.m_memoryAllocationListOffset], ax
 
+    ; Initialize the child list
+    mov es:[di + ts_process.m_childListSegment], ax
+    mov es:[di + ts_process.m_childListOffset], ax
+
     pop ax
     pop dx
     pop di
@@ -71,6 +77,104 @@ process_new:
     
     %undef p_parentSegment
     %undef p_parentOffset
+
+; void *process_destroy(struct ts_process *p_process)
+process_destroy:
+    %define p_processOffset (bp + 4)
+    %define p_processSegment (bp + 6)
+
+    push bp
+    mov bp, sp
+
+    ; Destroy threads
+    .initThreadDestroyLoop:
+        les di, [p_processOffset]
+        les di, es:[di + ts_process.m_threadListOffset]
+
+    .threadDestroyLoop:
+        mov ax, es
+        or ax, di
+        jz .threadDestroyLoopEnd
+
+        push word es:[di + ts_listElement.m_dataSegment]
+        push word es:[di + ts_listElement.m_dataOffset]
+        call thread_destroy
+        add sp, 4
+
+        push es
+        push di
+        les di, es:[di + ts_listElement.m_nextOffset]
+        call free
+        add sp, 4
+
+        jmp .threadDestroyLoop
+
+    .threadDestroyLoopEnd:
+        
+    .initMemoryAllocationDestroyLoop:
+        les di, [p_processOffset]
+        les di, es:[di + ts_process.m_memoryAllocationListOffset]
+
+    .memoryAllocationDestroyLoop:
+        mov ax, es
+        or ax, di
+        jz .memoryAllocationDestroyLoopEnd
+
+        push es
+        push di
+
+        les di, es:[di + ts_listElement.m_dataOffset]
+
+        push es
+        push di
+
+        ; Free memory
+        xor ax, ax
+        push ax
+        push word es:[di + ts_memoryAllocation.m_segmentCount]
+        push word es:[di + ts_memoryAllocation.m_segment]
+        call mm_mark
+        add sp, 6
+
+        ; Free memory allocation
+        call free
+        add sp, 4
+
+        ; Free list element
+        pop di
+        pop es
+        les di, es:[di + ts_listElement.m_nextOffset]
+        sub sp, 4
+        call free
+        add sp, 4
+
+        jmp .memoryAllocationDestroyLoop
+
+    .memoryAllocationDestroyLoopEnd:
+
+    .removeFromParentSubprocessList:
+        les di, [p_processOffset]
+        les di, es:[di + ts_process.m_parentOffset]
+        lea ax, [di + ts_process.m_childListOffset]
+        push word [p_processSegment]
+        push word [p_processOffset]
+        push es
+        push ax
+        call list_remove
+        add sp, 8
+
+    .destroyProcess:
+        push word [p_processOffset]
+        push word [p_processSegment]
+        call free
+        add sp, 4
+
+    .end:
+        pop bp
+        ret
+
+    %undef p_processOffset
+    %undef p_processSegment
 
 section .data
 g_process_nextPid: dw 1
